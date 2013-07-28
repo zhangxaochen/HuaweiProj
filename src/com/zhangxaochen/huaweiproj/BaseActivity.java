783c9aff8979932591c57d6b9957c4ea6641c07c
Chen ZHANG <zhangxaochen@163.com>
2013-07-28 12:13:22 +0800

    v1.4.0 - +assert os4.0; +assert sensors exist

--- src/com/zhangxaochen/huaweiproj/BaseActivity.java
+++ src/com/zhangxaochen/huaweiproj/BaseActivity.java
@@ -3,11 +3,15 @@ package com.zhangxaochen.huaweiproj;
 import java.io.File;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.DialogInterface.OnDismissListener;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.SystemClock;
@@ -23,8 +27,67 @@ public class BaseActivity extends Activity {
 	int _sampleRate=60;
 	String _debugInfo;
 	String _fileName=Environment.getExternalStorageDirectory().getAbsolutePath()
-			+File.separator+"huawei.xml";;
-
+			+File.separator+"huawei.xml";
+	
+				
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+		//------------------------检测 android 系统版本
+		if(android.os.Build.VERSION.SDK_INT<Build.VERSION_CODES.ICE_CREAM_SANDWICH){
+			System.out.println("shit~~~~~~~~~~~~~~~~~~");
+			String errorMsg="需要 Android OS4.0 以上版本的系统";
+			AlertDialog.Builder builder=new AlertDialog.Builder(this);
+			builder.setTitle("系统要求")
+			.setMessage(errorMsg)
+			.setCancelable(false)
+			.setPositiveButton("退出程序", new OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					BaseActivity.super.onBackPressed();
+				}
+			})
+			.show();
+		}
+		
+		
+		//------------------------检测传感器硬件是否存在
+		PackageManager pm=this.getPackageManager();
+		boolean hasAcc=pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_ACCELEROMETER);
+		boolean hasGyro=pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_GYROSCOPE);
+		boolean hasMag=pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_COMPASS);
+		
+		if(!(hasAcc && hasGyro && hasMag)){
+			String errorMsg=
+					"重力感应：\t"+(hasAcc?"有":"缺失")+"\n"+
+					"陀螺仪：\t\t"+(hasGyro?"有":"缺失")+"\n"+
+					"电子罗盘：\t"+(hasMag?"有":"缺失")+"\n\n"+
+					"~~~请使用同时具备以上三个传感器模块的设备进行数据采集~~~";
+			AlertDialog.Builder builder=new AlertDialog.Builder(this);
+			builder.setTitle("！缺少运动传感器模块！")
+			.setMessage(errorMsg)
+			.setCancelable(false)
+			.setPositiveButton("退出程序", new OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+//					BaseActivity.this.finish();
+					BaseActivity.super.onBackPressed();					
+					
+				}
+			});
+			
+			Dialog lackSensorDlg=builder.create();
+			lackSensorDlg.show();	
+			
+		}
+			
+			
+		
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
